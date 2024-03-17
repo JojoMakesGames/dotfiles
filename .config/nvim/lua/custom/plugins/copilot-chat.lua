6a540c2be02c286c6db709adf5078fb72cf4032d
JojoMakesGames <jojo.oatey@gmail.com>
2024-03-17 14:22:50 -0400

    feat: add trouble, copilot-chat, ts-error-translator plugins

---
+++ .config/nvim/lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
